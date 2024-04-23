e81b5f0e63efa15fdb838af23e35b18ed7119554
EnesMCLK <40140673+EnesMCLK@users.noreply.github.com>
2024-04-23 19:45:00 +0300

    Delete leafletJS/js directory
    Signed-off-by: EnesMCLK <40140673+EnesMCLK@users.noreply.github.com>

--- leafletJS/js/map.js
+++
@@ -1,5 +0,0 @@
-var map = L.map('map').setView([40.7, 29.9], 10);
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '© OpenStreetMap'
-}).addTo(map);
